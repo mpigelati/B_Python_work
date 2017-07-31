e08ce7410cbb8b3c69a9b23c5c0d26842fef2add
mpigelati <saimohansai@gmail.com>
2017-07-31 15:13:54 +0530

    adding few more changes

--- Assain/Process/Subprocess.py
+++ Assain/Process/Subprocess.py
@@ -1,9 +1,6 @@
 import subprocess
+import os
 #import subprocess32 as sub
-'''
-with open("file1.txt","a") as f:
-    f.flush()
-    sub.Popen(["cat","temp.txt"],stdout=f)
 
 
 try :
@@ -15,18 +12,16 @@ except:
 else :
     print("file opened successfilly")
 
+cmd =["ls", "-la"]
+subprocess.call(cmd, stdout=fd)
+#out.close()
 
-subprocess.call(["ls","-la >" ," >"], stdout=fd)
-subprocess.call(["ls","-la >"],stdout=fd)
+#subprocess.call(["ls","-la >"],stdout=fd)
 
-subprocess.call(["ls","-la >"], stdout=fd)
+#subprocess.call(["ls","-la >"], stdout=fd)
 
 
-input_files = ['file1', 'file2', 'file3']
-my_cmd = ['cat'] + input_files
-with open('myfile', "w") as outfile:
-subprocess.call(my_cmd, stdout=outfile)'''
 
 #subprocess.call('du -hs $HOME', shell=True)
 #subprocess.call(["ls","-la",">samp.txt"],shell=True)
-subprocess.call('ls -la >samp.txt', shell=True)
+#subprocess.call('ls -la >samp.txt', shell=True)
