84d9339229078885a331d388d162202fd16c9ff0
mpigelati <saimohansai@gmail.com>
2017-08-08 23:06:28 +0530

    completed telnet work

--- Assain/Telnet/my_files/Telnet_project_Thread.py
+++ Assain/Telnet/my_files/Telnet_project_Thread.py
@@ -1,12 +1,18 @@
 import telnetlib
 import json
 import unicodedata
+import thread
+import os
 
-def Fun_Telnet(localhost,user,password):
-
+def Fun_Telnet (localhost,user,password):
+    print(thread.get_ident())
     HOST = localhost.encode('ascii', 'ignore')
     user = user.encode('ascii', 'ignore')
     password = password.encode('ascii', 'ignore')
+
+    #HOST ='localhost'
+    #user ="moamsai"
+    #password ="jnjnuhuh"
 
     print(user)
     tn = telnetlib.Telnet(HOST)
@@ -28,6 +34,15 @@ def Fun_Telnet(localhost,user,password):
 json_file = "data.json"
 json_data = open(json_file)
 data = json.load(json_data)
-
+print(os.getpid())
 for i in range(0,len(data)):
-    Fun_Telnet(data[i]["Host"],data[i]["user"],data[i]["password"])
+
+    try:
+        thread.start_new_thread(Fun_Telnet, (data[i]["Host"],data[i]["user"],data[i]["password"]))
+    except:
+        print("failed to create thread")
+    else:
+        print("sucessfully started thread")
+
+while 1:
+    pass
