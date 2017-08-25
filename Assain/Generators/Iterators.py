63216cf422ffa893613707aa01598fc94b958efa
mpigelati <saimohansai@gmail.com>
2017-08-25 19:55:05 +0530

    adding doc for iterator and generator

---
+++ Assain/Generators/Iterators.py
@@ -0,0 +1,37 @@
+'''
+    Python iterator objects are required to support two methods while following the iterator protocol.
+iter :- __iter__ returns the iterator object itself. This is used in for and in statements.
+
+next :- __next__ method returns the next value from the iterator. If there is no more items to return then it should raise StopIteration exception
+
+'''
+# define a list
+my_list = [4, 7, 0, 3]
+
+# get an iterator using iter()
+my_iter = iter(my_list)
+
+## iterate through it using next()
+
+#prints 4
+print(next(my_iter))
+
+#prints 7
+print(next(my_iter))
+
+## next(obj) is same as obj.__next__()
+
+#prints 0
+print(my_iter.__next__())
+
+#prints 3
+print(my_iter.__next__())
+
+## This will raise error, no items left
+next(my_iter)
+
+"""
+Traceback (most recent call last):
+  File "/home/mohansai/python/B_Python_work/Assain/Generators/Iterators.py", line 31, in <module>
+    next(my_iter)
+StopIteration"""
--- Assain/Generators/yeald.py
+++ Assain/Generators/yeald.py
@@ -1,4 +1,12 @@
+'''
+It is fairly simple to create a generator in Python.
+It is as easy as defining a normal function with yield statement instead of a return statement.
+If a function contains at least one yield statement (it may contain other yield or return statements),
+it becomes a generator function. Both yield and return will return some value from a function.
+The difference is that, while a return statement terminates a function entirely,
+ yield statement pauses the function saving all its states and later continues from there on successive calls.'''
 
+"""
 def generatots(num):
     n=0
     while(n<num):
@@ -7,4 +15,20 @@ def generatots(num):
 
 gen =generatots(10)
 for i in gen:
-    print(i)
+    print(i) """
+
+def fibonacci():
+    """Fibonacci numbers generator"""
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+f = fibonacci()
+
+counter = 0
+for x in f:
+    print (x),
+    counter += 1
+    if (counter > 10): break
+print
