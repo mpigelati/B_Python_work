d8b03df7df463d7c767a960f2763e79b7d002ec4
mpigelati <saimohansai@gmail.com>
2017-08-25 19:59:44 +0530

    adding some more logic to itrrators

--- Assain/Generators/Iterators.py
+++ Assain/Generators/Iterators.py
@@ -5,6 +5,7 @@ iter :- __iter__ returns the iterator object itself. This is used in for and in 
 next :- __next__ method returns the next value from the iterator. If there is no more items to return then it should raise StopIteration exception
 
 '''
+'''
 # define a list
 my_list = [4, 7, 0, 3]
 
@@ -28,10 +29,23 @@ print(my_iter.__next__())
 print(my_iter.__next__())
 
 ## This will raise error, no items left
-next(my_iter)
+next(my_iter)'''
 
 """
 Traceback (most recent call last):
   File "/home/mohansai/python/B_Python_work/Assain/Generators/Iterators.py", line 31, in <module>
     next(my_iter)
 StopIteration"""
+
+my_list = [4, 7, 0, 3]
+iter_obj = iter(my_list)
+
+# infinite loop
+while True:
+    try:
+        # get the next item
+        element = next(iter_obj)
+        # do something with element
+    except StopIteration:
+        # if StopIteration is raised, break from loop
+        break
